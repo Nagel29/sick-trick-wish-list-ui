70b0d7ee69f05dbb99ccb8708d623fb0c5a3752e
Ryan Nagel <RNagel29@gmail.com>
2023-01-02 16:00:08 -0700

    Add cypress testing

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,57 @@
 describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  beforeEach(() => {
+    cy.intercept('http://localhost:3001/api/v1/tricks', {
+      method: 'GET',
+      fixture: '../fixtures/tricks.json',
+    })
+    cy.visit('localhost:3000')
   })
+
+  it('should display the header', () => {
+    cy.get('h1').should('contain', 'Sick Trick Wish List')
+  })
+
+  it('should display the form', () => {
+    cy.get('#form').should('exist')
+    cy.get('#stance').should('contain', 'Choose Your Stance')
+    cy.get('#name').should('exist')
+    cy.get('#obstacle').should('contain', 'Choose Your Obstacle')
+    cy.get('#tutorial').should('exist')
+    cy.get('button').should('exist')
+  })
+
+  it('should display the initial tricks', () => {
+    cy.get('.trick').eq(0).should('contain', 'regular')
+      .and('contain', 'kickflip')
+      .and('contain', 'Obstacle: flat ground')
+      .and('contain', 'Link to Tutorial')
+      .and('contain', 'www.learn2skate.com/1')
+    cy.get('.trick').eq(1).should('contain', 'switch')
+      .and('contain', 'heelflip')
+      .and('contain', 'Obstacle: stairs')
+      .and('contain', 'Link to Tutorial')
+      .and('contain', 'www.learn2skate.com/2')
+  })
+
+  it('should update form values as changed', () => {
+    cy.get('#stance').select('Regular').should('contain', 'Regular')
+    cy.get('#name').type('Backflip').should('have.value', 'Backflip')
+    cy.get('#obstacle').select('Stairs').should('contain', 'Stairs')
+    cy.get('#tutorial').type('www.learn2skate.com/3').should('have.value', 'www.learn2skate.com/3')
+  })
+
+  it('should add the trick once the form is submitted', () => {
+    cy.get('#stance').select('Regular')
+    cy.get('#name').type('Backflip')
+    cy.get('#obstacle').select('Stairs')
+    cy.get('#tutorial').type('www.learn2skate.com/3')
+    cy.get('button').click()
+    cy.get('.trick').eq(2).should('contain', 'Regular')
+    .and('contain', 'Backflip')
+    .and('contain', 'Obstacle: Stairs')
+    .and('contain', 'Link to Tutorial')
+    .and('contain', 'www.learn2skate.com/3')
+  })
+
+
 })
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -19,14 +19,14 @@ class Form extends Component {
 
     render() {
         return (
-            <form>
-                <select name='stance' value={this.state.stance} onChange={this.handleChange}>
+            <form id='form'>
+                <select id='stance' name='stance' value={this.state.stance} onChange={this.handleChange}>
                     <option value="Choose Your Stance" disabled hidden>Choose Your Stance</option>
                     <option value="Regular">Regular</option>
                     <option value="Switch">Switch</option>
                 </select>
-                <input name='name' placeholder='Name of Trick' value={this.state.name} onChange={this.handleChange}></input>
-                <select name='obstacle' value={this.state.obstacle} onChange={this.handleChange}>
+                <input id='name' name='name' placeholder='Name of Trick' value={this.state.name} onChange={this.handleChange}></input>
+                <select id='obstacle' name='obstacle' value={this.state.obstacle} onChange={this.handleChange}>
                     <option value="Choose Your Obstacle" disabled hidden>Choose Your Obstacle</option>
                     <option value="Flatground">Flatground</option>
                     <option value="Ledge">Ledge</option>
@@ -34,7 +34,7 @@ class Form extends Component {
                     <option value="Stairs">Stairs</option>
                     <option value="Pool">Pool</option>
                 </select>
-                <input name='tutorial' placeholder='Link to Tutorial' value={this.state.tutorial} onChange={this.handleChange}></input>
+                <input id='tutorial' name='tutorial' placeholder='Link to Tutorial' value={this.state.tutorial} onChange={this.handleChange}></input>
                 <button onClick={(event) => {
                     event.preventDefault()
                     this.handleSubmit(this.state)
